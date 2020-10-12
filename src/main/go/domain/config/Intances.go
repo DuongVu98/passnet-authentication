938aaf4e12987bda18df9c5c1ecdf11b5dc6fcc3
Tung Duong <duongvutung1998@gmail.com>
2020-10-12 23:56:47 +0700

    clean app config

--- src/main/go/domain/config/Intances.go
+++ src/main/go/domain/config/Intances.go
@@ -1,18 +1,12 @@
 package config
 
 import (
-	firebase "firebase.google.com/go/v4"
 	myproto "github.com/DuongVu98/passnet-authentication/src/main/gen/src/main/proto"
 	"github.com/DuongVu98/passnet-authentication/src/main/go/domain/repository"
-	"github.com/go-bongo/bongo"
-	"gopkg.in/mgo.v2"
 )
 
 type (
 	AppConfig struct {
-		FirebaseApp          firebase.App
-		BongoClient          *bongo.Connection
-		MgoClient            *mgo.Session
 		MessageServiceClient myproto.MessageServiceClient
 	}
 	BeanConfig struct {
