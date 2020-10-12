c648fc74ff8238dd854d503199667f2ded5cdad3
Tung Duong <duongvutung1998@gmail.com>
2020-10-13 02:25:58 +0700

    create saga client

--- src/main/go/adapter/grpc/MessageService.go
+++ src/main/go/adapter/grpc/MessageService.go
@@ -4,26 +4,23 @@ import (
 	"context"
 	"github.com/DuongVu98/passnet-authentication/src/main/gen/src/main/proto"
 	"github.com/DuongVu98/passnet-authentication/src/main/go/adapter/models"
+	"github.com/DuongVu98/passnet-authentication/src/main/go/config/app"
 	"github.com/DuongVu98/passnet-authentication/src/main/go/domain/channels"
-	"google.golang.org/grpc"
 	"log"
 	"reflect"
 )
 
 var grpcChannel = channels.GetGrpcChannel()
 
+var appConfig = app.GetAppConfigInstance()
+var sagaMessageClient = appConfig.MessageServiceClient
+
 func ProcessMessage() {
 	message := <-grpcChannel
 	switch reflect.TypeOf(message).String() {
 	case reflect.TypeOf(&models.CreateUserMessage{}).String():
 		log.Printf("Create User!!")
+		_, _ = sagaMessageClient.SendCreateUserMessage(context.Background(), &proto.CreateUserMessage{Uid: message.(*models.CreateUserMessage).Uid})
 	}
-}
-
-type MessageServiceClient struct {
-}
-
-func (m MessageServiceClient) SendCreateUserMessage(ctx context.Context, in *proto.CreateUserMessage, opts ...grpc.CallOption) (*proto.Response, error) {
-	panic("implement me")
 }
 
