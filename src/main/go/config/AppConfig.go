56143a19e53494ad5372103c0c779ee67bd4a033
Tung Duong <duongvutung1998@gmail.com>
2020-10-20 22:51:47 +0700

    remove set-env command + refractor beanconfig

--- src/main/go/config/AppConfig.go
+++ src/main/go/config/AppConfig.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/DuongVu98/passnet-authentication/src/main/go/config/app"
-	"github.com/DuongVu98/passnet-authentication/src/main/go/config/bean"
 	"github.com/DuongVu98/passnet-authentication/src/main/go/config/handles"
 	"github.com/DuongVu98/passnet-authentication/src/main/go/domain/channels"
 	"github.com/DuongVu98/passnet-authentication/src/main/go/domain/config"
@@ -21,13 +20,13 @@ func RunAppConfig() {
 		appConfigChannel <- appConfigInstance
 	}()
 
-	handles.ChannelHandler()
 	handles.RunAllGrpcChannelProcesses()
 
 	//Bean config
-	beanConfigInstance := bean.GetBeanConfigInstance()
+	beanConfigInstance := &config.BeanConfig{}
 	beanConfigInstance.UserRepository = app.GetUserMgmRepository()
 	singletonFactory.Set("beanConfig", beanConfigInstance)
+
 	// Global config
 	app.SetMgmClient()
 
