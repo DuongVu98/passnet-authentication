197ae74bbe4bcfe181d4a5a251630b18b680ecd5
Tony <duongvutung1998@gmail.com>
2020-12-24 17:59:39 +0700

    try to test mongo

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,20 +1,22 @@
 print(
-	"Start #################################################################"
+    "Start #################################################################"
 );
 
 db = db.getSiblingDB("passnet_auth");
+db.dropUser("passnet-auth-app");
 db.createUser({
-	user: "passnet-auth-app",
-	pwd: "tungduong98",
-	roles: [
-		{
-			role: "readWrite",
-			db: "passnet_auth",
-		},
-	],
+    user: "passnet-auth-app",
+    pwd: "tungduong98",
+    roles: [
+        {
+            role: "readWrite",
+            db: "passnet_auth",
+        },
+        {role: "readAnyDatabase", db: "admin"}
+    ],
 });
 
 db.createCollection("test");
-db.test.insert({ key: "value" });
+db.test.insert({key: "value"});
 
 print("END #################################################################");
